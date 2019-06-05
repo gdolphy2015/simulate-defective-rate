a6426e3455453509b1701f5c786a8071759f925a
Grace Dolphy <gdolphy2015@my.fit.edu>
2019-06-05 01:17:54 -0400

    Create copyMachine.m

---
+++ copyMachine.m
@@ -0,0 +1,32 @@
+# Given a copy machine that is broken today, find out the state of the copy machine after one month.
+# Given that if it is broken, there is a 15% chance that it will still be broken the next day,
+# and if it is working today, then there is a 95% chance it will work the next day. 
+
+total = 0;
+
+%number of experiemnts
+numExperiements = 1000000;
+
+for j = 1:1:numExperiements 
+    state = 0; %currently broken
+        
+    desiredDay = 30;
+    
+    for i = 1:1:desiredDay
+        if (state == 0) %not working
+            if (not (rand() <= .15))
+               state = 1; 
+            end
+        else %working
+            if (not (rand() <= .95))
+                state = 0;
+            end
+        end
+    end
+    
+    if (state == 1) %ends up working.
+        total = total + 1;
+    end
+end
+
+total/numExperiements
