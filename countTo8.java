4cf5443df8ea66c504412d003557f475c17d4562
Grace Dolphy <gdolphy2015@my.fit.edu>
2019-08-25 23:20:51 -0400

    Create countTo8.java

---
+++ countTo8.java
@@ -0,0 +1,21 @@
+/*
+ * Finding the number of solutions to x1 + x2 + x3 = 8, using only non-negative integers.
+ */
+
+public class countTo8 {
+
+	public static void main(String[] args) {
+		int counter = 0;
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				for (int k =0 ; k < 10; k++) {
+					if (i + j + k == 8) {
+						System.out.println(i + " + " + j + " + " + k);
+						counter++;
+					}
+				}
+			}
+		}	
+		System.out.println(counter);
+	}
+}
